c0fd8cfcaf9e769308e9a3dafc9e16cadedda671
vadim <asd@toto.kz>
2020-09-04 16:30:39 +0600

    mobile

---
+++ src/js/app.js
@@ -0,0 +1,78 @@
+"use strict";
+
+// const slider = tns({
+//     container: '.carousel__inner',
+//     items: 1,
+//     speed: 500,
+//     responsive: {
+//         640: {
+//           edgePadding: 20,
+//           gutter: 20,
+//           items: 1
+//         },
+//         700: {
+//           gutter: 30
+//         },
+//         900: {
+//           items: 1
+//         }
+//       },
+//     slideBy: 'page',
+//     autoplay: false,
+//     controls: false,
+//     nav: false
+// });
+
+
+
+// document.querySelector('.prev').addEventListener('click', function () {
+//     slider.goTo('prev');
+// });
+
+// document.querySelector('.next').addEventListener('click', function () {
+//     slider.goTo('next');
+// });
+// $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+//   $(this)
+//     .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+//     .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('active');
+// });
+
+
+$(document).ready(function(){
+  $('.carousel__inner').slick({
+      speed: 1200,
+      adaptiveHeight: true,
+      prevArrow: '<button type="button" class="prev"><img src="icons/left.svg"></button>',
+      nextArrow: '<button type="button" class="next"><img src="icons/right.svg"></button>',
+      responsive: [
+          {
+              breakpoint: 992,
+              settings: {
+                  dots: true,
+                  arrows: false
+              }
+          }
+      ]
+  });
+  
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+      $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+
+
+  function toggleSlide(item) {
+    $(item).each(function(i) {
+      $(this).on('click', function(e) {
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+      });
+    });
+  };
+
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
+});
