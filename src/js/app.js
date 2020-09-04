6f9aad23bbacba94fdf93ff585cb7660f3e0bc5f
vadim <asd@toto.kz>
2020-09-04 20:27:15 +0600

    mobile

--- src/js/app.js
+++ src/js/app.js
@@ -42,17 +42,30 @@
 $(document).ready(function(){
   $('.carousel__inner').slick({
       speed: 1200,
-      adaptiveHeight: true,
+      adaptiveHeight: false,
       prevArrow: '<button type="button" class="prev"><img src="icons/left.svg"></button>',
       nextArrow: '<button type="button" class="next"><img src="icons/right.svg"></button>',
       responsive: [
           {
-              breakpoint: 992,
+            breakpoint: 992,
               settings: {
-                  dots: true,
-                  arrows: false
+                arrows: true,
+                infinite: true,
+                speed: 300,
+                slidesToShow: 1,
+                adaptiveHeight: false
               }
-          }
+          },
+          {
+            breakpoint: 767,
+            settings: {
+              arrows: true,
+              infinite: true,
+              speed: 300,
+              slidesToShow: 1,
+              adaptiveHeight: false
+            }
+        },
       ]
   });
   
