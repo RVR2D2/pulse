4a70d4accbf47060c7b9f3cf3fc03162800680f5
vadim <asd@toto.kz>
2020-09-06 23:00:05 +0600

    modal window

--- src/js/app.js
+++ src/js/app.js
@@ -1,44 +1,5 @@
 "use strict";
 
-// const slider = tns({
-//     container: '.carousel__inner',
-//     items: 1,
-//     speed: 500,
-//     responsive: {
-//         640: {
-//           edgePadding: 20,
-//           gutter: 20,
-//           items: 1
-//         },
-//         700: {
-//           gutter: 30
-//         },
-//         900: {
-//           items: 1
-//         }
-//       },
-//     slideBy: 'page',
-//     autoplay: false,
-//     controls: false,
-//     nav: false
-// });
-
-
-
-// document.querySelector('.prev').addEventListener('click', function () {
-//     slider.goTo('prev');
-// });
-
-// document.querySelector('.next').addEventListener('click', function () {
-//     slider.goTo('next');
-// });
-// $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
-//   $(this)
-//     .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-//     .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('active');
-// });
-
-
 $(document).ready(function(){
   $('.carousel__inner').slick({
       speed: 1200,
@@ -88,4 +49,20 @@ $(document).ready(function(){
 
   toggleSlide('.catalog-item__link');
   toggleSlide('.catalog-item__back');
+
+  // Modal
+
+  $('[data-modal=consultation]').on('click', function() {
+    $('.overlay, #consultation').fadeIn('slow');
+});
+$('.modal__close').on('click', function() {
+    $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+});
+
+$('.button_mini').each(function(i) {
+    $(this).on('click', function() {
+        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn('slow');
+    });
+});
 });
