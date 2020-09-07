0416a7b8aa7dc31a385bdda02cf5dccc29a9ab3f
vadim <asd@toto.kz>
2020-09-07 22:19:11 +0600

    mobile  v2

--- src/js/app.js
+++ src/js/app.js
@@ -1,45 +1,44 @@
 "use strict";
 
-$(document).ready(function(){
+$(document).ready(function () {
   $('.carousel__inner').slick({
-      speed: 1200,
-      adaptiveHeight: false,
-      prevArrow: '<button type="button" class="prev"><img src="icons/left.svg"></button>',
-      nextArrow: '<button type="button" class="next"><img src="icons/right.svg"></button>',
-      responsive: [
-          {
-            breakpoint: 992,
-              settings: {
-                arrows: true,
-                infinite: true,
-                speed: 300,
-                slidesToShow: 1,
-                adaptiveHeight: false
-              }
-          },
-          {
-            breakpoint: 767,
-            settings: {
-              arrows: true,
-              infinite: true,
-              speed: 300,
-              slidesToShow: 1,
-              adaptiveHeight: false
-            }
-        },
-      ]
+    speed: 1200,
+    adaptiveHeight: false,
+    prevArrow: '<button type="button" class="prev"><img src="icons/left.svg"></button>',
+    nextArrow: '<button type="button" class="next"><img src="icons/right.svg"></button>',
+    responsive: [{
+        breakpoint: 992,
+        settings: {
+          arrows: true,
+          infinite: true,
+          speed: 300,
+          slidesToShow: 1,
+          adaptiveHeight: false
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          arrows: true,
+          infinite: true,
+          speed: 300,
+          slidesToShow: 1,
+          adaptiveHeight: false
+        }
+      },
+    ]
   });
-  
-  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
-      $(this)
-        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
   });
 
 
   function toggleSlide(item) {
-    $(item).each(function(i) {
-      $(this).on('click', function(e) {
+    $(item).each(function (i) {
+      $(this).on('click', function (e) {
         e.preventDefault();
         $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
         $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
@@ -52,17 +51,45 @@ $(document).ready(function(){
 
   // Modal
 
-  $('[data-modal=consultation]').on('click', function() {
+  $('[data-modal=consultation]').on('click', function () {
     $('.overlay, #consultation').fadeIn('slow');
-});
-$('.modal__close').on('click', function() {
+  });
+  $('.modal__close').on('click', function () {
     $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
-});
+  });
 
-$('.button_mini').each(function(i) {
-    $(this).on('click', function() {
-        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
-        $('.overlay, #order').fadeIn('slow');
+  $('.button_mini').each(function (i) {
+    $(this).on('click', function () {
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+      $('.overlay, #order').fadeIn('slow');
     });
-});
+  });
+
+ 
+  function valideteForms(form) {
+    $(form).validate({
+      rules: {
+        name: 'required',
+        phone: 'required',
+        email: {
+          required: true,
+          email: true
+        }
+      },
+      messages: {
+        name: {
+          required: "Пожалуйста, введите свое имя",
+          minlength: jQuery.validator.format("Введите {0} символа!")
+        },
+        phone: "Пожалуйста, введите свой номер телефона",
+        email: {
+          required: "Пожалуйста, введите свою почту",
+          email: "Неправильно введен адрес почты"
+        }
+      }
+    });
+  };
+  valideteForms('#consultation-form');
+  valideteForms('#consultation form');
+  valideteForms('#order form');
 });
