c492d0d31dc374a06960bc65e546d9ad58db604e
vadim <asd@toto.kz>
2020-09-08 22:45:20 +0600

    form php

--- src/js/app.js
+++ src/js/app.js
@@ -92,4 +92,22 @@ $(document).ready(function () {
   valideteForms('#consultation-form');
   valideteForms('#consultation form');
   valideteForms('#order form');
+
+  $('input[name=phone]').mask('+ 7 (999) 999-99-99');
+
+  $('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+      type: 'POST',
+      url: 'mailer/smart.php',
+      data: $(this).serialize()
+    }).done(function() {
+      $(this).find('input').val('');
+      $('#consultation, #order').fadeOut();
+      $('.overlay, #thanks').fadeIn('slow');
+
+      $('form').trigger('reset');
+    });
+    return false;
+  });
 });
