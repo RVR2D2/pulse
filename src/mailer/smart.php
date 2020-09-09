46547fb9b6c70d1dc77cf41989dd46a315e25417
vadim <asd@toto.kz>
2020-09-09 22:09:34 +0600

    animate css3

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -13,13 +13,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'poom_peii@mail.ru';                 // Наш логин
-$mail->Password = 'Boomer2222';                           // Наш пароль от ящика
+$mail->Username = 'poompeii01@gmail.com';                 // Наш логин
+$mail->Password = 'BoomeR2222';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
 $mail->setFrom('', 'Pulse');   // От кого письмо 
-$mail->addAddress('poompeii01@gmail.com');     // Add a recipient
+$mail->addAddress('');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
